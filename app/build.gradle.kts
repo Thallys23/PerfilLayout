fd2764ae8a9aff43215ec86f64ffa779a400e95a
2Bmodular <2bmod@bglab.local>
2025-11-18 21:29:32 -0300

    Finalização da tela de perfil pessoal

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.perfilpessoal"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.perfilpessoal"
